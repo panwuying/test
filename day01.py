4777a9cb28fe225b8253b50b4bb4fe3169a1e66a
root <root@localhost.localdomain>
2018-09-25 04:00:37 -0400

    1

---
+++ day01.py
@@ -0,0 +1,19 @@
+
+'''print("hello word")
+num= raw_input ("input chang>>")
+num1=raw_input ("input kuan>>")
+int_num=int(num)
+int_num1=int(num1)
+area=int_num*int_num1
+print(area)
+int_a=500
+int_b=500/60
+int_c=500%60
+print(int_b ,"fen",int_c,"miao")'''
+a=int(raw_input(">>"))
+b=int(raw_input(">>"))
+c=int(raw_input(">>"))
+x=int(raw_input(">>"))
+y=int(raw_input(">>"))
+l=int(3+4*x)/5-10*(y-5)(a+b+c)/x+9*(4/x+(9+x)/y)
+print(l)
