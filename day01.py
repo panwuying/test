7455472ed9bd9ebe336572d7a9843cb2717e838b
root <root@localhost.localdomain>
2018-09-25 04:32:46 -0400

    1

--- day01.py
+++ day01.py
@@ -1,5 +1,6 @@
 
-'''print("hello word")
+'''lianxi
+print("hello word")
 num= raw_input ("input chang>>")
 num1=raw_input ("input kuan>>")
 int_num=int(num)
@@ -9,11 +10,22 @@ print(area)
 int_a=500
 int_b=500/60
 int_c=500%60
-print(int_b ,"fen",int_c,"miao")'''
+print(int_b ,"fen",int_c,"miao")
 a=int(raw_input(">>"))
 b=int(raw_input(">>"))
 c=int(raw_input(">>"))
 x=int(raw_input(">>"))
 y=int(raw_input(">>"))
 l=int(3+4*x)/5-10*(y-5)(a+b+c)/x+9*(4/x+(9+x)/y)
-print(l)
+print(l)'''
+'''1
+C=int(raw_input("Entera degree in celsius>>"))
+F=float(9.0/5.0)*C+32
+print(F)'''
+'''2'''
+radius,length=float(raw_input("Enter the radius and length of a cylinder:>>")
+area = float (radius*radius*3.14)
+volume =float (area*length)
+print("the area is",area)
+print("the volume is".volume)
+
